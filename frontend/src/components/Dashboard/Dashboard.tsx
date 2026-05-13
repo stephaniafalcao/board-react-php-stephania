974d269de2cd451a2c6df17434eedb0a1a59d77f
stephaniafalcao <stephaniafalcao35025@gmail.com>
2026-05-13 12:18:26 -0300

    feat: Criado sidebar. Adicionada lib lucide-react para utilizar icones

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
+import { Sidebar } from "../Sidebar/Sidebar";
 import "./dashboard.css";
 
 export function Dashboard() {
   return (
     <main className="dashboard-layout">
+      <Sidebar />
       <section className="dashboard-content">
       </section>
     </main>
