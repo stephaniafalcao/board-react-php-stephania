f255b2f5e7b6576515ef6ff52d1ff440822a53f8
stephaniafalcao <stephaniafalcao35025@gmail.com>
2026-05-13 12:31:14 -0300

    feat: Criado component DashboardHeader.

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,13 @@
 import { Sidebar } from "../Sidebar/Sidebar";
 import "./dashboard.css";
+import { DashboardHeader } from "./DashboardHeader";
 
 export function Dashboard() {
   return (
     <main className="dashboard-layout">
       <Sidebar />
       <section className="dashboard-content">
+        <DashboardHeader />
       </section>
     </main>
   );
